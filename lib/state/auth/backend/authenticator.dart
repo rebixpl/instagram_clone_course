43aa78cd326deffb2d15db6243df493a36803fd5
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-06-28 21:39:20 +0200

    implemented authenticator
    loginWithFacebook + logOut

---
+++ lib/state/auth/backend/authenticator.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagram_clone_course/state/auth/constants/constants.dart';
+import 'package:instagram_clone_course/state/auth/models/auth_result.dart';
+import 'package:instagram_clone_course/state/posts/typedefs/user_id.dart';
+
+class Authenticator {
+  User? get currentUser => FirebaseAuth.instance.currentUser;
+
+  UserId? get userId => currentUser?.uid;
+  bool get isAlreadyLoggedIn => userId != null;
+  String get displayName => currentUser?.displayName ?? '';
+  String? get email => currentUser?.email;
+
+  Future<void> logOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    await FacebookAuth.instance.logOut();
+  }
+
+  Future<AuthResult> loginWithFacebook() async {
+    final loginResult = await FacebookAuth.instance.login();
+    final token = loginResult.accessToken?.token;
+    if (token == null) {
+      // The user has aborted the login process.
+      return AuthResult.aborted;
+    }
+
+    final oauthCredential = FacebookAuthProvider.credential(token);
+    try {
+      await FirebaseAuth.instance.signInWithCredential(
+        oauthCredential,
+      );
+      return AuthResult.success;
+    } on FirebaseAuthException catch (e) {
+      // It could mean that users account could exist on different oauth provider server.
+      // for example facebook account with same email address already exists as google account.
+      final email = e.email;
+      final credential = e.credential;
+      if (e.code == Constants.accountExistsWithDifferentCredential &&
+          email != null &&
+          credential != null) {
+        // we wanna get the previous providers user u sed to log in with this email
+        final providers =
+            await FirebaseAuth.instance.fetchSignInMethodsForEmail(
+          email,
+        );
+
+        if (providers.contains(Constants.googleCom)) {
+          // we wanna log in with google
+          await loginWithGoogle();
+          // we wanna link the facebook credential to the google account
+          currentUser?.linkWithCredential(credential);
+        }
+        return AuthResult.success;
+      }
+      return AuthResult.failure;
+    }
+  }
+
+  Future<AuthResult> loginWithGoogle() async {
+    return AuthResult.failure;
+  }
+}
