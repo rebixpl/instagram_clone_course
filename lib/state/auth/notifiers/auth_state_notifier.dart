24c4a50f6721d29dbcb44d6362d494d6ad7f62f0
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-06-29 17:38:25 +0200

    login with google + login with facebook

--- lib/state/auth/notifiers/auth_state_notifier.dart
+++ lib/state/auth/notifiers/auth_state_notifier.dart
@@ -2,9 +2,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone_course/state/auth/backend/authenticator.dart';
 import 'package:instagram_clone_course/state/auth/models/auth_result.dart';
 import 'package:instagram_clone_course/state/auth/models/auth_state.dart';
+import 'package:instagram_clone_course/state/posts/typedefs/user_id.dart';
+import 'package:instagram_clone_course/state/user_info/backend/user_info_storage.dart';
 
 class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
+  final _userInfoStorage = const UserInfoStorage();
 
   AuthStateNotifier() : super(const AuthState.unknown()) {
     if (_authenticator.isAlreadyLoggedIn) {
@@ -25,5 +28,41 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
   Future<void> loginWithGoogle() async {
     state = state.copiedWithIsLoading(true);
     final result = await _authenticator.loginWithGoogle();
+    final userId = _authenticator.userId;
+
+    // We want to save the user info only if the login was successful and we have a userId.
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(userId: userId);
+    }
+
+    state = AuthState(
+      result: result,
+      isLoading: false,
+      userId: userId,
+    );
   }
+
+  Future<void> loginWithFacebook() async {
+    state = state.copiedWithIsLoading(true);
+    final result = await _authenticator.loginWithFacebook();
+    final userId = _authenticator.userId;
+
+    // We want to save the user info only if the login was successful and we have a userId.
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(userId: userId);
+    }
+
+    state = AuthState(
+      result: result,
+      isLoading: false,
+      userId: userId,
+    );
+  }
+
+  Future<void> saveUserInfo({required UserId userId}) =>
+      _userInfoStorage.saveUserInfo(
+        userId: userId,
+        displayName: _authenticator.displayName,
+        email: _authenticator.email,
+      );
 }
